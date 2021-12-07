8e2e4ee47c09ef9800bffb3294b5571f0a75d7b8
Mark Nunez <39011339+marnugue@users.noreply.github.com>
2021-12-07 14:15:14 +0100

    Delete pp.py

--- utils/pp.py
+++
@@ -1,7 +0,0 @@
-import spacy
-nlp = spacy.load("es_core_news_sm")
-
-text = 'Los gatos y los perros juegan jugaban jugado juntos en el patio de pierde pierden su casa'
-doc = nlp(text)
-for word in doc:
-    print(word.lemma_,word)
