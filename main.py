b7eaf91b90c762c293cf32336293159e59f2c693
marnugue <=>
2021-12-12 20:51:23 +0100

    order probs

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ if __name__ == '__main__':
     # Opcional si se quiere extraer los ficheros html de salud,deporte y extraer su info. En caso de descomentar la
     # linea poner en cleanTexts el atributo extractor=True
 
-    html = HTMLextractor("sources")
-    html.extract()
-    documentExtractor.extract()
+    # html = HTMLextractor("sources")
+    # html.extract()
+    # documentExtractor.extract()
 
     # Opciones de limpieza por defecto lematizer=False stopword=True,steaming = True
     data = cleanTexts("data/dataset",extractor=True)
