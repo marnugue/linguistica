1eaff71d1710223149830e63ebd41a75cf2b60d5
marnugue <marcostorterra@gmail.com>
2021-12-15 19:22:53 +0100

    update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from utils.cleaner import cleanTexts
 from utils.model import Model
 from utils import documentExtractor
-from utils.ExtractorHTML import HTMLextractor
+from utils.extractorHTML import HTMLextractor
 import pickle
 
 models = ["bayes_bow","regresion_bow","bayes_tfidf","regresion_tfidf"]
@@ -11,9 +11,9 @@ if __name__ == '__main__':
     # Opcional si se quiere extraer los ficheros html de salud,deporte y extraer su info. En caso de descomentar la
     # linea poner en cleanTexts el atributo extractor=True
     
-    # html = HTMLextractor("sources")
-    # html.extract()
-    # documentExtractor.extract()
+    html = HTMLextractor("sources")
+    html.extract()
+    documentExtractor.extract()
 
     # Opciones de limpieza por defecto lematizer=False stopword=True,steaming = True
     print("\n________ Modelos stemmer ________")
